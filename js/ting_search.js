f9092d016eb83a782c98bec7551dc709f2b82b0e
DQ26525 <DQ26525@LT4750.Randers.dk>
2017-08-15 12:09:34 +0200

    Small fix

--- js/ting_search.js
+++ js/ting_search.js
@@ -6,13 +6,13 @@
     $('input[name="search_block_form"]').keydown(function(event) {
       // When enter is hit in the search form.
       if (event.which === 13) {
-        Drupal.TingSearchOverlay();
+        //Drupal.TingSearchOverlay();
       }
     });
 
     // Hook into the search button as well.
     $('#search-block-form input[type="submit"]').click(function() {
-      Drupal.TingSearchOverlay();
+      //Drupal.TingSearchOverlay();
 
       return true;
     });
@@ -20,7 +20,7 @@
     // Add search link to the different links on the search result page.
     $('.search-results a').live('click', function() {
       if ($(this).not('[target="_blank"]').length) {
-        Drupal.TingSearchOverlay();
+        //Drupal.TingSearchOverlay();
       }
     });
   });
